0d1434c8c4364b4bede61771700117ba12c8a2ba
Jey <126204426+Jeyrama@users.noreply.github.com>
2023-11-10 10:22:19 -0800

    Update Triple-trouble.js

--- Triple-trouble.js
+++ Triple-trouble.js
@@ -17,3 +17,12 @@ Examples:
 
 
 // Solution
+
+function tripledouble(num1, num2) {
+  for(let i = 0; i < 10; i++) {
+    if (new RegExp(`${i}{3}`).test(num1) && new RegExp(`${i}{2}`).test(num2)) {
+      return 1;
+    }
+  }
+  return 0;
+}
